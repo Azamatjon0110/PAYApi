bf3094fe3cf15102ef01610f0483014b7c84e558
developer-773 <galaxylogin@yandex.com>
2022-08-08 10:52:34 +0500

    first pull request

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// function ShowMessage(firstName)
+// {
+//     function SayHello() {
+//         alert("Hello " + firstName);
+//     }
+
+//     return SayHello();
+// }
+
+// ShowMessage("Steve");
